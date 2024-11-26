c3fb9a56dd1d5e5f70562133e6fd76b6beda3b82
Felix <felavalos@gmail.com>
2024-11-26 12:01:48 -0600

    Change fetch functions to axios functions

--- api/api-client.ts
+++ api/api-client.ts
@@ -1,45 +1,52 @@
-import { ArtistResource } from '@/types/artist';
+import axios from 'axios';
+import { Artist, ArtistResource } from '@/types/artist';
 
-const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
-const URLBase = process.env.EXPO_PUBLIC_API_URL;
+const api_url = process.env.EXPO_PUBLIC_API_URL as string;
+const api_key = process.env.EXPO_PUBLIC_API_KEY;
 
-function getMusicData() {
-  return fetch(`${ URLBase }geo.gettopartists&country=spain&api_key=${API_KEY}&format=json`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(response => response.json())
-  .then(data => data.topartists.artist)
-  .then(artists => artists.map( (artist: ArtistResource) => {
-    return {
-      id: artist.mbid,
-      name: artist.name,
-      image: artist.image[0]['#text']
-    }
-  } ))
-  .catch((error) => console.log(error));
+const getTopArtist = async ( country: string ) => {
+  const params = {
+    method: 'geo.gettopartists',
+    country: country,
+    api_key: api_key,
+    format: 'json'
+  };
+
+  try {
+    const { data } = await axios.get( api_url, { params } );
+    const artists = data.topartists.artist;
+
+    return artists.map( (artist: ArtistResource) => {
+      return {
+        id: artist.mbid,
+        name: artist.name,
+        image: artist.image[0]["#text"]
+      }
+    });
+
+  } catch (error) {
+    console.error('Error fetching top artists: ', error);
+  }
+};
+
+const getArtistData = async ( mbid: string ) => {
+  const params = {
+    method: 'artist.getinfo',
+    mbid: mbid,
+    api_key: api_key,
+    format: 'json'
+  };
+
+  const { data } = await axios.get( api_url, { params } );
+  const artist = data.artist;
+
+  return {
+    name: artist.name,
+    image: artist.image
+  };
+};
+
+export {
+  getTopArtist,
+  getArtistData
 }
-
-function getArtistData( mbid: string ) {
-  return fetch(`${ URLBase }artist.getinfo&mbid=${mbid}&api_key=${API_KEY}&format=json`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
-  .then( response => response.json() )
-  .then( data => data.artist )
-  .then( artist => {
-    return {
-      name: artist.name,
-      image: artist.image[0]['#text']
-    }
-  })
-  .catch((error) => console.log(error));
-}
-
-export { getMusicData, getArtistData }
