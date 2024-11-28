202322c63e43391dac17085484ceb934ca43b041
Felix <felavalos@gmail.com>
2024-11-28 15:21:31 -0600

    Wrap app/_layout.tsx with tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,24 @@
 // Libraries
 import { Stack } from "expo-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-      headerTitleAlign: 'center'
-    }}>
-      <Stack.Screen
-        name="index"
-        options={{ title: "Login" }}
-      />
-      <Stack.Screen
-        name="tabs"
-        options={{ headerShown: false }}
-      />
-    </Stack>
+    <QueryClientProvider client={ queryClient }>
+      <Stack screenOptions={{
+        headerTitleAlign: 'center'
+      }}>
+        <Stack.Screen
+          name="index"
+          options={{ title: "Login" }}
+        />
+        <Stack.Screen
+          name="tabs"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </QueryClientProvider>
   );
 }
