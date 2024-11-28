29464af4f7cadaee715d4431e7aa60d8c3132c9a
Felix <felavalos@gmail.com>
2024-11-27 20:23:07 -0600

    User data api response

--- types/user.ts
+++ types/user.ts
@@ -1,10 +1,34 @@
-export interface User {
-  key: string;
-  name: string;
-}
-
-export interface UserAPIResponse {
+export interface UserSession {
   key: string;
   name: string;
   subscriber: number;
+}
+
+export interface User {
+  name:         string;
+  age:          string;
+  subscriber:   string;
+  realname:     string;
+  bootstrap:    string;
+  playcount:    string;
+  artist_count: string;
+  playlists:    string;
+  track_count:  string;
+  album_count:  string;
+  image:        Image[];
+  registered:   Registered;
+  country:      string;
+  gender:       string;
+  url:          string;
+  type:         string;
+}
+
+export interface Image {
+  size:    string;
+  "#text": string;
+}
+
+export interface Registered {
+  unixtime: string;
+  "#text":  number;
 }
